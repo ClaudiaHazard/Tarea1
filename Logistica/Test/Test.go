6ff6f9b584e26b6f6318e49490775fac0f975e5b
Claudia <claudia.hazard.14@sansano.usm.cl>
2020-10-12 20:02:38 -0300

    servidores test

--- Logistica/Test/Test.go
+++ Logistica/Test/Test.go
@@ -1,6 +1,10 @@
 package test
 
+import (
+	servidorcliente "github.com/ClaudiaHazard/Tarea1/Logistica/ServidorCliente"
+)
+
 //SayHello Test
 func test() {
-	servidorcliente.servidorcliente()
+	servidorcliente.IniciarServidorCliente()
 }
