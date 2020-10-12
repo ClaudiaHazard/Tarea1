298a603ebbd5c1f7fb8dbc0e980cc6cac62d8a26
Claudia <claudia.hazard.14@sansano.usm.cl>
2020-10-11 23:55:12 -0300

    "Cliente" funcionando, ahora a probar...

--- Camiones/Camiones.go
+++ Camiones/Camiones.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"log"
+
 	"fmt"
+
+	enviapaquete "github.com/ClaudiaHazard/Tarea1/Logistica/EnviaPaquete/EnviaPaqueteGo"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 )
 
 const (
-	address     = "localhost:50051"
-	defaultName = "world"
+	port = "50051"
 )
 
 func main() {
 	fmt.Println("Helloww world")
 
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial(":"+port, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.Close()
+
+	c := enviapaquete.NewConexionServiceClient(conn)
+
+	response, err := c.SayHello(context.Background(), &enviapaquete.Message{Body: "Hello From Client!"})
+	if err != nil {
+		log.Fatalf("Error when calling SayHello: %s", err)
+	}
+	log.Printf("Response from server: %s", response.Body)
 }
