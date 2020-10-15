5c7fed03b956077c495952e04b6e6a5374c3daac
Claudia <claudia.hazard.14@sansano.usm.cl>
2020-10-15 00:24:25 -0300

    Funcionando, logistica y funciones asociadas

--- Logistica/Logistica.go
+++ Logistica/Logistica.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 
+	sm "github.com/ClaudiaHazard/Tarea1/ServicioMensajeria"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
@@ -42,33 +43,33 @@ type Server struct {
 }
 
 //EntregaPosicion recibe paquete de Camiones en Logistica
-func (s *Server) EntregaPosicion(ctx context.Context, in *InformacionPaquete) (*logistica.Message, error) {
+func (s *Server) EntregaPosicion(ctx context.Context, in *sm.InformacionPaquete) (*sm.Message, error) {
 	log.Printf("Receive message body from client: %d", in.CodigoSeguimiento)
-	return &Message{Body: "Hola desde Logistica!"}, nil
+	return &sm.Message{Body: "Hola desde Logistica!"}, nil
 }
 
 //InformaEntrega recibe paquete de Camiones en Logistica
-func (s *Server) InformaEntrega(ctx context.Context, in *Message) (*Message, error) {
+func (s *Server) InformaEntrega(ctx context.Context, in *sm.Message) (*sm.Message, error) {
 	log.Printf("Receive message body from client: %s", in.Body)
-	return &Message{Body: "Hola desde Logistica!"}, nil
+	return &sm.Message{Body: "Hola desde Logistica!"}, nil
 }
 
 //RecibeInstrucciones recibe paquete de Camiones en Logistica
-func (s *Server) RecibeInstrucciones(ctx context.Context, in *Message) (*Paquete, error) {
+func (s *Server) RecibeInstrucciones(ctx context.Context, in *sm.Message) (*sm.Paquete, error) {
 	log.Printf("Receive message body from client: %s", in.Body)
-	return &Paquete{}, nil
+	return &sm.Paquete{}, nil
 }
 
 //RealizaOrden recibe paquete de Camiones en Logistica
-func (s *Server) RealizaOrden(ctx context.Context, in *Orden) (*CodSeguimiento, error) {
+func (s *Server) RealizaOrden(ctx context.Context, in *sm.Orden) (*sm.CodSeguimiento, error) {
 	log.Printf("Receive message body from client: %s", in.Nombre)
-	return &CodSeguimiento{}, nil
+	return &sm.CodSeguimiento{}, nil
 }
 
 //SolicitaSeguimiento recibe paquete de Camiones en Logistica
-func (s *Server) SolicitaSeguimiento(ctx context.Context, in *CodSeguimiento) (*Estado, error) {
+func (s *Server) SolicitaSeguimiento(ctx context.Context, in *sm.CodSeguimiento) (*sm.Estado, error) {
 	log.Printf("Receive message body from client: %d", in.CodigoSeguimiento)
-	return &Estado{Estado: "Bonito"}, nil
+	return &sm.Estado{Estado: "Bonito"}, nil
 }
 
 //Para usar en local, cambiar ipport por ":"+port
@@ -85,7 +86,7 @@ func main() {
 
 	fmt.Println("En espera de Informacion paquetes")
 
-	RegisterMensajeriaServiceServer(grpcServer, &s)
+	sm.RegisterMensajeriaServiceServer(grpcServer, &s)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve gRPC server over "+ipport+": %v", err)
