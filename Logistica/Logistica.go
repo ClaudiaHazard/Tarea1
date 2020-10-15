dbebba19f316f81baf248ff2dd82c133dfd38dc2
Claudia <claudia.hazard.14@sansano.usm.cl>
2020-10-15 00:17:59 -0300

    change

--- Logistica/Logistica.go
+++ Logistica/Logistica.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net"
 
-	serviciomensajeria "github.com/ClaudiaHazard/Tarea1/ServicioMensajeria"
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
 
@@ -25,15 +25,50 @@ func IniciaServidor() {
 
 	grpcServer := grpc.NewServer()
 
-	s := serviciomensajeria.Server{}
+	s := Server{}
 
 	fmt.Println("En espera de Informacion paquetes")
 
-	serviciomensajeria.RegisterMensajeriaServiceServer(grpcServer, &s)
+	sm.RegisterMensajeriaServiceServer(grpcServer, &s)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve gRPC server over "+ipport+": %v", err)
 	}
+}
+
+//Server simple
+type Server struct {
+	//id int
+}
+
+//EntregaPosicion recibe paquete de Camiones en Logistica
+func (s *Server) EntregaPosicion(ctx context.Context, in *InformacionPaquete) (*logistica.Message, error) {
+	log.Printf("Receive message body from client: %d", in.CodigoSeguimiento)
+	return &Message{Body: "Hola desde Logistica!"}, nil
+}
+
+//InformaEntrega recibe paquete de Camiones en Logistica
+func (s *Server) InformaEntrega(ctx context.Context, in *Message) (*Message, error) {
+	log.Printf("Receive message body from client: %s", in.Body)
+	return &Message{Body: "Hola desde Logistica!"}, nil
+}
+
+//RecibeInstrucciones recibe paquete de Camiones en Logistica
+func (s *Server) RecibeInstrucciones(ctx context.Context, in *Message) (*Paquete, error) {
+	log.Printf("Receive message body from client: %s", in.Body)
+	return &Paquete{}, nil
+}
+
+//RealizaOrden recibe paquete de Camiones en Logistica
+func (s *Server) RealizaOrden(ctx context.Context, in *Orden) (*CodSeguimiento, error) {
+	log.Printf("Receive message body from client: %s", in.Nombre)
+	return &CodSeguimiento{}, nil
+}
+
+//SolicitaSeguimiento recibe paquete de Camiones en Logistica
+func (s *Server) SolicitaSeguimiento(ctx context.Context, in *CodSeguimiento) (*Estado, error) {
+	log.Printf("Receive message body from client: %d", in.CodigoSeguimiento)
+	return &Estado{Estado: "Bonito"}, nil
 }
 
 //Para usar en local, cambiar ipport por ":"+port
@@ -46,11 +81,11 @@ func main() {
 
 	grpcServer := grpc.NewServer()
 
-	s := serviciomensajeria.Server{}
+	s := Server{}
 
 	fmt.Println("En espera de Informacion paquetes")
 
-	serviciomensajeria.RegisterMensajeriaServiceServer(grpcServer, &s)
+	RegisterMensajeriaServiceServer(grpcServer, &s)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve gRPC server over "+ipport+": %v", err)
