2650c3951d6787a1e253ea41bcdf643945993fe6
samuelmarinn <55009185+samuelmarinn@users.noreply.github.com>
2020-10-18 22:04:07 -0300

    cambio ip

--- Logistica/Logistica.go
+++ Logistica/Logistica.go
@@ -21,8 +21,8 @@ import (
 const (
 	//ipportgrpc = "10.6.40.162:50051"
 	ipportgrpc = ":50051"
-	//ipportrabbitmq = "amqp://test:test@10.6.40.1:5672/"
-	ipportrabbitmq = "amqp://guest:guest@localhost:5672/"
+	ipportrabbitmq = "amqp://test:test@10.6.40.162:5672/"
+	//ipportrabbitmq = "amqp://guest:guest@localhost:5672/"
 )
 
 //error handling
